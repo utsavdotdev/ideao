a7375668dc251d99b0ddbd552420f6018fd4e13a
Utsav bhattarai <95236817+utsavbhattarai007@users.noreply.github.com>
2022-08-15 09:01:16 +0545

    Navbar toggle menu coded

--- client/src/component/Navbar.jsx
+++ client/src/component/Navbar.jsx
@@ -1,28 +1,54 @@
-import React, {  useContext } from "react";
+import React, { useContext } from "react";
 import "../css/Navbar.css";
 import { RiLightbulbFlashLine as Logo } from "react-icons/ri";
 import { ContextProvider } from "../config/Context";
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 import LoginModal from "./LoginModal";
+import { AiOutlineUser } from "react-icons/ai";
+import { IoLogOutOutline } from "react-icons/io5";
 function Navbar() {
   const { mdl } = useContext(ContextProvider);
   const [modal, setModal] = mdl;
+  const menuToggle = () =>{
+    const toggleMenu = document.querySelector('.menu')
+    toggleMenu.classList.toggle('active')
+  }
   return (
     <>
-    {modal && <LoginModal/>}
+      {modal && <LoginModal />}
       <div className="nav_container">
         <div className="left">
           <div className="logo">
             <Logo size="25" />
           </div>
-          <Link to="/"><h3 className="logo_text">Ideao</h3></Link>
+          <Link to="/">
+            <h3 className="logo_text">Ideao</h3>
+          </Link>
         </div>
         <div className="right">
-          <button className="btn" onClick={() => setModal(!modal)}>
-            <p className="login">
-              Login
-            </p>
-          </button>
+          {/* <button className="btn" onClick={() => setModal(!modal)}>
+            <p className="login">Login</p>
+          </button> */}
+          <div className="login_avatar_wrapper">
+            <img
+              src="https://lh3.googleusercontent.com/-stWcQqcBZIQ/AAAAAAAAAAI/AAAAAAAAAAA/AHYzNgpIfU1ph3OVWPLlSQKrdKyolIxlUg/photo.jpg?sz=46"
+              alt="avatar"
+              className="login_avatar"
+              onClick={menuToggle}
+            />
+            <div className="menu">
+              <ul>
+                <li>
+                  <AiOutlineUser className="navbar_link" />
+                  <Link to="/profile" className="navbar_a">My Profile</Link>
+                </li>
+                <li>
+                  <IoLogOutOutline className="navbar_link" />
+                  <p className="navbar_a">Sign Out</p>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
     </>
